a4093b4993dc698b556cba220ae4409cdb996b54
Mark Polak <mark@thedutchies.com>
2014-07-30 17:49:47 +0200

    Update dist with crumb update

--- dist/js/d2.js
+++ dist/js/d2.js
@@ -273,6 +273,10 @@ d2BreadCrumbs.service('breadCrumbsService', function () {
     this.addCrumb = function (name, callback) {
         var crumb = {};
 
+        if ( ! angular.isString(name) || name === '') {
+            return;
+        }
+
         crumb.name = name;
         crumb.id = this.crumbsList.length;
 
