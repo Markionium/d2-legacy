9f014f9aa1b6c9b225fa920a201cac867cb06d7a
Mark Polak <mark@thedutchies.com>
2014-09-16 13:07:33 +0200

    Update header directive documentation

--- src/common/recordtable/recordtable-header-directive.js
+++ src/common/recordtable/recordtable-header-directive.js
@@ -11,12 +11,18 @@
  *
  * This directive represents the column headers as they are displayed by the {@link recordTable} directive.
  *
+ * #Sorting
+ *
  * The directive adds the sort functionality and calls the setSortOrder function on {@link RecordTableController}.
  *
+ * #Searchable columns
  * An input box with be added to the column header when `column.searchable` is set to true.
  *
- * When typeahead is available it asks for the typeahead values on {@link RecordTableController} through the `typeAheadCache` property.
+ * A class can be configured in the tableConfig that is passed to the recordTable directive to add a class to the input boxes.
+ * When adding a property `headerInputClass` to the tableConfig this class will be added to the input searchboxes.
  *
+ * #Typeahead
+ * When typeahead is available it asks for the typeahead values on {@link RecordTableController} through the `typeAheadCache` property.
  */
 function recordTableHeader() {
     return {
